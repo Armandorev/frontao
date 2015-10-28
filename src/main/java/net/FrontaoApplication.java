4c6bdc7bac855ca03c268c3c3a6363d4415f39a2
Armando Sánchez Medina <armandosanchezmedina@gmail.com>
2015-10-28 21:23:28 +0000

    Added Spring initializr

---
+++ src/main/java/net/FrontaoApplication.java
@@ -0,0 +1,12 @@
+package net.;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontaoApplication.class, args);
+    }
+}
